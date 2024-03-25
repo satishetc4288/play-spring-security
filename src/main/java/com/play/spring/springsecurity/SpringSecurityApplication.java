7a1bc6260f2f38ab7feb39306ddab03ac40787b1
satish rajput <satishetc4288@gmail.com>
2024-03-25 20:48:20 +0530

    basic auth enabled

--- src/main/java/com/play/spring/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/play/spring/springsecurity/SpringSecurityApplication.java
@@ -2,8 +2,10 @@ package com.play.spring.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SpringSecurityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
---
+++ src/main/java/com/play/spring/springsecurity/controller/BaseController.java
@@ -0,0 +1,17 @@
+package com.play.spring.springsecurity.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BaseController {
+
+    @GetMapping("/greet")
+    public ResponseEntity<String> greetings(@RequestParam String name){
+        return ResponseEntity.ok("Hello, " + name);
+    }
+}
